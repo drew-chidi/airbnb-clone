e374cfff4f231396317d7e18a83b03962768a1b4
drew-chidi <chidi.andrew@gmail.com>
2023-07-10 15:23:24 +0100

    added empty state

--- app/page.js
+++ app/page.js
@@ -1,8 +1,50 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import Container from '@/app/components/Container';
+import ListingCard from '@/app/components/listings/ListingCard';
+import EmptyState from '@/app/components/EmptyState';
 
-const inter = Inter({ subsets: ['latin'] });
+import getListings, { IListingsParams } from '@/app/actions/getListings';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import ClientOnly from './components/ClientOnly';
 
-export default function Home() {
-  return <div>Hello Airbnb</div>;
-}
+const Home = async ({ searchParams }) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      // <ClientOnly>
+      <EmptyState showReset />
+      // </ClientOnly>
+    );
+  }
+
+  return (
+    // <ClientOnly>
+    <Container>
+      <div
+        className='
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+      >
+        {listings.map((listing) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+    // </ClientOnly>
+  );
+};
+
+export default Home;
