d3d72668313a44818667dd591156026263b69d58
drew-chidi <chidi.andrew@gmail.com>
2023-07-12 06:47:00 +0100

    minor fix

--- app/actions/getCurrentUser.js
+++ app/actions/getCurrentUser.js
@@ -1,37 +1,37 @@
-// import { getServerSession } from 'next-auth/next';
+// // import { getServerSession } from 'next-auth/next';
 
-// import { authOptions } from "@/pages/api/auth/[...nextauth]";
-// import prisma from "@/app/libs/prismadb";
+// // import { authOptions } from "@/pages/api/auth/[...nextauth]";
+// // import prisma from "@/app/libs/prismadb";
 
-export async function getSession() {
-  //   return await getServerSession(authOptions);
-}
+// export async function getSession() {
+//   //   return await getServerSession(authOptions);
+// }
 
-export default async function getCurrentUser() {
-  try {
-    const session = await getSession();
+// export default async function getCurrentUser() {
+//   try {
+//     const session = await getSession();
 
-    if (!session?.user?.email) {
-      return null;
-    }
+//     if (!session?.user?.email) {
+//       return null;
+//     }
 
-    const currentUser = await prisma.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
+//     const currentUser = await prisma.user.findUnique({
+//       where: {
+//         email: session.user.email,
+//       },
+//     });
 
-    if (!currentUser) {
-      return null;
-    }
+//     if (!currentUser) {
+//       return null;
+//     }
 
-    return {
-      ...currentUser,
-      createdAt: currentUser.createdAt.toISOString(),
-      updatedAt: currentUser.updatedAt.toISOString(),
-      emailVerified: currentUser.emailVerified?.toISOString() || null,
-    };
-  } catch (error) {
-    return null;
-  }
-}
+//     return {
+//       ...currentUser,
+//       createdAt: currentUser.createdAt.toISOString(),
+//       updatedAt: currentUser.updatedAt.toISOString(),
+//       emailVerified: currentUser.emailVerified?.toISOString() || null,
+//     };
+//   } catch (error) {
+//     return null;
+//   }
+// }
